2a4e56e734c83f8695e6976a0012744129ac95d2
DavideGila <davide.gila@gmail.com>
2023-11-15 15:18:18 +0100

    created MainComponent with v-for to make appear movies and series

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <h2>Movies</h2>
+    <div v-for="movie in store.movieList"> {{ movie.title }}</div>
+
+    <h2>Series</h2>
+    <div v-for="serie in store.seriesList"> {{ serie.name }}</div>
+</template>
+
+<script>
+import {store} from '../data/store.js'
+    export default {
+        name: 'MainComponent',
+        data() {
+            return {
+                store
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
