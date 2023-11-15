a9782a44b947215c83a0243a02d4246e79749f2c
DavideGila <davide.gila@gmail.com>
2023-11-15 14:30:29 +0100

    created store

--- src/data/store.js
+++ src/data/store.js
@@ -1,1 +1,16 @@
-a9f12e55fa53586e59311102e6a5b433
+import {reactive} from 'vue';
+export const store = reactive({
+    apiUrl: 'https://api.themoviedb.org/3/'
+    endPoint: {
+        movie: 'search/movie',
+        series: 'search/tv'
+    },
+    params: {
+        api_key: 'a9f12e55fa53586e59311102e6a5b433',
+        query: 'a'
+    },
+    error: '',
+    loading: true,
+    movieList:[],
+    seriesList:[]
+})
