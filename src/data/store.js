5e61b865c04034d30e3e212ce7feb9a58a560ae2
DavideGila <davide.gila@gmail.com>
2023-11-15 15:00:41 +0100

    Milestone 1 completed

--- src/data/store.js
+++ src/data/store.js
@@ -1,6 +1,6 @@
 import {reactive} from 'vue';
 export const store = reactive({
-    apiUrl: 'https://api.themoviedb.org/3/'
+    apiUrl: 'https://api.themoviedb.org/3/',
     endPoint: {
         movie: 'search/movie',
         series: 'search/tv'
