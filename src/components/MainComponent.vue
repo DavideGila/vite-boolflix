173414666a4de3fb5345cb3fa9426cd22482c47d
DavideGila <davide.gila@gmail.com>
2023-11-15 17:38:20 +0100

    created props for movies and series

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,23 +1,49 @@
 <template>
-    <h2>Movies</h2>
-    <div v-for="movie in store.movieList"> {{ movie.title }}</div>
-
-    <h2>Series</h2>
-    <div v-for="serie in store.seriesList"> {{ serie.name }}</div>
+    <div>
+        <img :src="movieImg" :alt="movieTitle" class="w-100"/>
+        <div class="text-center bg-cards h">
+            <h5 class="title">{{ movieTitle }}</h5>
+            <h6 class="title">{{ movieOriginalTitle }}</h6>
+            <p>{{ movieLanguage }}</p>
+            <p>{{ movieVote }}</p>
+        </div>
+    </div>
+    <div>
+        <div>
+            <img :src="seriesImg" :alt="seriesTitle" class="w-100"/>
+            <div class="text-center bg-cards h">
+                <h5 class="title">{{ seriesTitle }}</h5>
+                <h6 class="title">{{ seriesOriginalTitle }}</h6>
+                <p>{{ seriesLanguage }}</p>
+                <p>{{ seriesVote }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import {store} from '../data/store.js'
-    export default {
-        name: 'MainComponent',
-        data() {
-            return {
-                store
-            }
-        },
-    }
+import { store } from '../data/store.js'
+export default {
+    name: 'MainComponent',
+    props: {
+        movieImg: String,
+        movieTitle: String,
+        movieOriginalTitle: String,
+        movieLanguage: String,
+        movieVote: Number,
+        seriesImg: String,
+        seriesTitle: String,
+        seriesOriginalTitle: String,
+        seriesLanguage: String,
+        seriesVote: Number
+    },
+    data() {
+        return {
+            store
+        }
+    },
+}
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
 </style>
