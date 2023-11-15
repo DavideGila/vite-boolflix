b7348c9300f565c6a6af48dd06564f03b898da6f
DavideGila <davide.gila@gmail.com>
2023-11-15 15:03:22 +0100

    imported axios and store and created a function to get movies and series

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,36 @@
 </template>
 
 <script>
+import axios from 'axios'
+import {store} from './data/store.js'
+
   export default {
-    
+    name: 'App',
+    data() {
+      return {
+        store
+      }
+    },
+    methods: {
+      getMoviesAndSeries(){
+        const movieUrl = store.apiUrl+store.endPoint.movie;
+        axios.get(movieUrl, {params: store.params}).then((response) => {
+          console.log(response.data.results);
+          store.movieList = response.data.results;
+        })
+        const seriesUrl = store.apiUrl+store.endPoint.series;
+        axios.get(seriesUrl, {params: store.params}).then((response) => {
+          console.log(response.data.results);
+          store.seriesList = response.data.results;
+        })
+      }
+    },
+    created(){
+      this.getMoviesAndSeries()
+    }
   }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 
 </style>
