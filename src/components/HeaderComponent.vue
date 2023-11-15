0731e71e3c1841dd64785aa4d310ad404787f646
DavideGila <davide.gila@gmail.com>
2023-11-15 15:54:09 +0100

    created HeaderComponent with a search bar and an input

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="d-flex justify-content-between align-items-center bg-dark">
+        <h1 class="text-danger">Boolflix</h1>
+        <div class="search-bar">
+            <div class="input-group input-group-sm mb-3">
+                <button class="input-group-text justify-content-center bg-white">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            <input type="text" class="form-control" placeholder="Cerca" v-model.trim="filter" />
+        </div>
+    </div>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'HeaderComponent'
+
+}
+</script>
+
+<style lang="scss" scoped></style>
