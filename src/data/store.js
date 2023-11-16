1a648b793da85e538ce5f439bf0b06db479f3e87
DavideGila <davide.gila@gmail.com>
2023-11-16 16:51:33 +0100

    created a new endpoint and a new param

--- src/data/store.js
+++ src/data/store.js
@@ -3,21 +3,26 @@ export const store = reactive({
     apiUrl: 'https://api.themoviedb.org/3/',
     endPoint: {
         movie: 'search/movie',
-        series: 'search/tv'
+        series: 'search/tv',
+        actorsMovie: 'movie/1895/credits'
     },
     params: {
         api_key: 'a9f12e55fa53586e59311102e6a5b433',
         query: 'star wars'
     },
+    paramsActor: {
+        api_key: 'a9f12e55fa53586e59311102e6a5b433'
+    },
     error: '',
     loading: true,
     movieList:[],
     seriesList:[],
+    actorsMovieList:[],
     flagsList:
         {
             en: '/images/Flag_of_the_United_Kingdom.png',
             fr: '/images/Flag_of_France.jpg',
             ja: '/images/Flag_of_Japan.jpg'
         },
-    imgLink: 'https://image.tmdb.org/t/p/w342/'
+    imgLink: 'https://image.tmdb.org/t/p/w342/',
 })
